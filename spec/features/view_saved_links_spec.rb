86ca801fe7586c80030f4fdec52d7c71da632ae7
Jenna Ramdenee <jenna_ramdenee@hotmail.co.uk>
2016-11-29 12:05:18 +0000

    Created feature test to view saved links

---
+++ spec/features/view_saved_links_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+RSpec.feature "View a link", :type=> :feature do
+
+  scenario "View a list of links" do
+    visit('/')
+    expect(page).to_have_text("Saved Links")
+  end
+
+end
