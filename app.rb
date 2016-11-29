1828c576e8ad8e75f13acc6b32512bbcc24e01aa
Oscar Barlow <oscar.barlow@gmail.com>
2016-11-29 15:21:11 +0000

    /links shows all links in database

--- app.rb
+++ app.rb
@@ -1,8 +1,17 @@
 require 'sinatra/base'
+require 'data_mapper'
+require 'dm-postgres-adapter'
+
+require_relative './app/models/link'
 
 class BookmarkManager < Sinatra::Base
   get '/' do
     'Saved Links'
+  end
+
+  get '/links' do
+    @links = Link.all
+    erb :links
   end
 
   # start the server if ruby file executed directly
