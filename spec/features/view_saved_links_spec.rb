fb0fdb5b84410c8b30440a79f01308062602f3ca
Oscar Barlow <oscar.barlow@gmail.com>
2016-11-29 13:36:13 +0000

    failing feature test to see existing links

--- spec/features/view_saved_links_spec.rb
+++ spec/features/view_saved_links_spec.rb
@@ -2,9 +2,15 @@ require 'spec_helper'
 
 RSpec.feature "View a link", :type=> :feature do
 
-  scenario "View a list of links" do
-    visit('/')
-    expect(page).to have_text("Saved Links")
+  scenario "See existing links" do
+    Link.create(url: 'http://www.makersacademy.com', title: 'Makers Academy')
+
+    visit('/links')
+    expect(page.status_code).to eq 200
+
+    within 'ul#links' do
+      expect(page).to have_content('Makers Academy')
+    end
   end
 
 end
