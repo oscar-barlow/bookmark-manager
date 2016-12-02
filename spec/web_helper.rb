4ccdc2fdf59e51eed6c6d8972580aedf7bad57cb
Oscar Barlow <oscar.barlow@gmail.com>
2016-12-02 11:28:07 +0000

    refactor submitting mismatched passwords to helper

--- spec/web_helper.rb
+++ spec/web_helper.rb
@@ -6,3 +6,11 @@ def sign_up
   fill_in :password_confirmation, with: 'Bryony'
   click_button 'Submit'
 end
+
+def sign_up_with_mismatched_confirmation
+  visit '/users/new'
+  fill_in :username, with: 'bryony@bryony.com'
+  fill_in :password, with: 'Bryony'
+  fill_in :password_confirmation, with: 'Oscar'
+  click_button('Submit')
+end
